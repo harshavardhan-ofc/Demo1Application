003a094405f223b85c2e537bd3709cf56be8e1da
harshavardhan-ofc <Harsha.Vardhan@triconinfotech.com>
2025-07-18 19:02:14 +0530

    removed name

--- demo1/src/main/java/com/postgresql/demo1/controller/CollectionsController.java
+++ demo1/src/main/java/com/postgresql/demo1/controller/CollectionsController.java
@@ -37,7 +37,7 @@ public class CollectionsController {
         hashset.add("Siddesh");
         hashset.add("gopi");
         hashset.add("ram");
-    System.out.println("getHashSet");
+    System.out.println("getHashSet :"+ name);
     int[] l = new int[] { 1,2,3};
         return hashset;
 
